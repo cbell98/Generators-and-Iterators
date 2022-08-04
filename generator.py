237b54afb687d5d9a42b5e4e2a5fbc47b484e6d4
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-08-04 15:27:43 -0400

    Update generator.py

--- generator.py
+++ generator.py
@@ -59,3 +59,39 @@ for index, roll in enumerate(d100, 1):
     print(index, roll)
     if index >= 10:
         break
+
+        
+# Mapping_Stars
+
+from datetime import datetime
+from itertools import starmap, count
+from typing import Dict
+
+from IteratorAlgorithms import generate
+
+
+def timestamp(data: Dict, label: str = "created") -> Dict:
+    data.update({label: datetime.now()})
+    return data
+
+
+def dict_to_str(data: Dict) -> str:
+    return "\n" + "\n".join(f"{k}: {v}" for k, v in data.items())
+
+
+counter = count(1)
+first_names = ("Ben", "Austin", "Woody", "John")
+last_names = generate(lambda: "Star")
+ages = range(36, 0, -2)
+
+array = (
+    {"firstname" : first, "lastname" : last, "age" : age}
+    for first, last, age in zip(first_names, last_names, ages)
+)
+
+creates = map(timestamp, array)
+updates = generate(lambda: "updated")
+results = starmap(timestamp, zip(creates, updates))
+
+for astronaut in results:
+    print(dict_to_str(astronaut))
